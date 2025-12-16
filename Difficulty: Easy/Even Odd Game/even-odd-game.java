8fca84a742f4fe56a7b9279f9ada23e9af4c8866
Md Sana <mailmdsana@gmail.com>
2025-12-16 22:09:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Even Odd Game/even-odd-game.java
@@ -0,0 +1,14 @@
+// User function Template for Java
+class Solution {
+    public static String utility(int n) {
+
+        String r = "";
+        if(n % 2 == 1){
+            r = "You";
+        }
+        else{
+            r = "Friend";
+        }
+        return r;
+    }
+}
