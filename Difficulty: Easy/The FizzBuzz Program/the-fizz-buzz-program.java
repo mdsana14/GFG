34731bf585ebc757960028484e1e46e60118c783
Md Sana <mailmdsana@gmail.com>
2025-12-27 18:31:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/The FizzBuzz Program/the-fizz-buzz-program.java
+++ Difficulty: Easy/The FizzBuzz Program/the-fizz-buzz-program.java
@@ -1,18 +1,22 @@
-// User function Template for Java
-class Solution {
-    public static void fizzBuzz(int number) {
-         
-         if(number % 3 == 0 && number % 5 == 0){
-             System.out.println("FizzBuzz");
-         }
-         else if(number % 3 == 0){
-             System.out.println("Fizz");
-         }
-         else if(number % 5 == 0){
-             System.out.println("Buzz");
-         }
-         else{
-             System.out.println(number);
-         }
+import java.util.*;
+
+class GFG {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if(a % 3 == 0 && a % 5 == 0){
+            System.out.println("FizzBuzz");
+        }
+        else if(a % 3 == 0){
+            System.out.println("Fizz");
+        }
+        else if(a % 5 == 0){
+            System.out.println("Buzz");
+        }
+        else{
+            System.out.println(a);
+        }
+        
     }
 }
