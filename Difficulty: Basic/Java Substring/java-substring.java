34e3c5aaa6a2336025a46e5650ded33002dee9ca
Md Sana <mailmdsana@gmail.com>
2026-01-14 19:42:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Java Substring/java-substring.java
@@ -0,0 +1,9 @@
+// User function template for Java
+class Solution {
+    static String javaSub(String S, int L, int R) {
+        String r = "";
+        r = S.substring(L,R+1);
+        return r;
+        
+    }
+}
