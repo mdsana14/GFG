29e0dc2e68cd60ec27e51723ad5f9bae7bab8a73
Md Sana <mailmdsana@gmail.com>
2026-01-14 19:51:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Inverted Right AngleTriangle/inverted-right-angle-triangle.java
@@ -0,0 +1,10 @@
+class Solution {
+    public void printPattern(int n) {
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
