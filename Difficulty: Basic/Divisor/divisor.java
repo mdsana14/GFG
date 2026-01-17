5823456c931460798f7fd910e5ca9bc444034da4
Md Sana <mailmdsana@gmail.com>
2026-01-17 18:19:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Divisor/divisor.java
@@ -0,0 +1,11 @@
+// User function Template for Java
+class Solution {
+    public static void divisor(int n) {
+         for(int i=1;i<=n;i++){
+            if(n % i == 0){
+                System.out.print(i + " ");
+            }
+         }
+        
+    }
+}
