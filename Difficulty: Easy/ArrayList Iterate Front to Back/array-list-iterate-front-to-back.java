9378c7d0ed3bd55491bcd1d107ab804c02e534e4
Md Sana <mailmdsana@gmail.com>
2026-02-02 18:12:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/ArrayList Iterate Front to Back/array-list-iterate-front-to-back.java
@@ -0,0 +1,9 @@
+// User function Template for Java
+class Solution {
+
+    public static void iterateArrayList(Iterator iterator) {
+        while(iterator.hasNext()){
+            System.out.print(iterator.next()+" ");
+        }
+    }
+}
